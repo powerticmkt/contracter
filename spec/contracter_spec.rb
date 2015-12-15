06d2472474723fc066aae4ec3b0bd25ea4bc0052
Remi Gelinas <remigelinas1@gmail.com>
2015-12-14 23:22:10 -0500

    N/A

--- spec/contracter_spec.rb
+++ spec/contracter_spec.rb
@@ -5,8 +5,6 @@ describe Contracter do
       context 'default contract' do
           it 'should not be null' do
               c = Contracter.build do
-                  company 'Remi Gelinas'
-                  date DateTime.now.strftime("%d:%m:%Y")
               end
 
               expect(c).to be_truthy
